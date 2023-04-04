ae596e97dea88ae15ea6f34b4b00959f1d44e35c
Clara <c.nahornyj@hotmail.fr>
2023-04-04 22:32:51 +0200

    Composants supprimés & modifiés

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ class Navbar extends Component {
     render() {
         return (
             <nav>
-                
+                <img src="https://upload.wikimedia.org/wikipedia/fr/1/11/Panda3D_Logo.png" alt="Logo de l'application" style={{"width":"50px"}}/>
             </nav>
         );
     }
