ab70c090808647eb5b2113c97a977ebd01ad74f5
Clara <clara.nahornyj@hotmail.com>
2023-08-20 23:58:44 +0200

    A l'initialisation du composant Home on récupère le state Destinations de Redux et on affiche en conséquence

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,31 +2,28 @@ import React, { Component } from 'react';
 import Navbar from '../components/Navbar';
 import DestinationsList from "../components/DestinationsList";
 import EmptyList from "../components/EmptyList";
-// TODO: récupérer les informations dans le localStorage et afficher le composant DestinationsList OU le composant EmptyList
+import { connect } from 'react-redux';
 
 class Home extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isDestinationsList: false
-        }
-    }
-    
-    componentDidMount(){
-        //TODO: on vérifie si le localStorage a un item places et si oui on affiche la liste des destinations
-        if(localStorage.getItem("placesId") !== null){
-            this.setState({isDestinationsList:true});
-        }
-    }
-        
+  
     render() {
         return (
             <div>
                 <Navbar/>
-                {this.state.isDestinationsList ? (<DestinationsList/>) : (<EmptyList/>)}
+                {this.props.destinations.length > 0 ? (<DestinationsList/>) : (<EmptyList/>)}
             </div>
         );
     }
+
+    componentDidMount(){
+        console.log(this.props.destinations);
+    }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+      destinations: state.destinations 
+    }
+  }
+
+export default connect(mapStateToProps, null)(Home);
