cad4b6156ebbaedebbb6789fd620d61fe6c36b42
Clara <clara.nahornyj@hotmail.com>
2023-08-21 00:03:41 +0200

    A l'initialisation on récupère le state Destinations de Redux et on affiche la/les destinations de l'utilisateur

--- src/components/DestinationsList.js
+++ src/components/DestinationsList.js
@@ -1,26 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import '../style/DestinationsList.css';
 
 class DestinationsList extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            destinationsList: []
-        }
-    }
-
-    componentDidMount(){
-        //TODO: faire un getItem sur le localStorage, convertir la donnée et MAJ l'état destinationsList avec ce qui est retourné
-        var places = localStorage.getItem('places');
-    }
-
-    render() {
-        return (
-            <div className='DestinationsList'>
-                <h1>Liste des destinations</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="DestinationsList">
+        <div className="HeaderDestinationList">
+          <h1>Vos destinations</h1>
+          <Link to={"/form"} className="redirection">
+            + Créer une destination
+          </Link>
+        </div>
+        <div className="List">
+        {this.props.destinations.map((destination) => (
+          <div className="Destination">
+            <p key={destination.city}>
+              {destination.city.toUpperCase()},{" "}
+              {destination.places[0].address_components[5].long_name}
+            </p>
+            <button>i</button>
+          </div>
+        ))}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default DestinationsList;
+const mapStateToProps = (state) => {
+  return {
+    destinations: state.destinations,
+  };
+};
+
+export default connect(mapStateToProps, null)(DestinationsList);
