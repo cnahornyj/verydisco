8517aa3deafd3b67d7621866c42bb1902f3b1e2d
Clara <clara.nahornyj@hotmail.com>
2023-08-25 16:56:14 +0200

    Ajout de l'action creator ADD_DESTINATION avec un objet destination pour l'action.payload, autres actions creators commentées

--- src/store.js
+++ src/store.js
@@ -4,46 +4,50 @@ const reduxDevtools =
 
 // state initial
 const initialState = {
-  city: null,
-  places: [],
   destinations: []
 };
 
 // actions creator
-export const addCity = (payload) => ({ type: "ADD_CITY" });
-export const prependPlace = (payload) => ({ type: "PREPEND_PLACE" });
-export const deletePlace = (payload) => ({ type: "DELETE_PLACE" });
-export const addDestinationAndDetails = (payload) => ({type: "ADD_DESTINATIONS" });
+// export const addCity = (payload) => ({ type: "ADD_CITY" });
+// export const prependPlace = (payload) => ({ type: "PREPEND_PLACE" });
+// export const deletePlace = (payload) => ({ type: "DELETE_PLACE" });
+// export const addDestinationAndDetails = (payload) => ({type: "ADD_DESTINATIONS" });
+export const addDestination = (payload) => ({type: "ADD_DESTINATION" });
 
 // reducer
 function placeReducer(state = initialState, action) {
     switch (action.type) {
-      case "ADD_CITY":
-            return {
-              ...state,
-              city: action.payload
-            };
-      case "PREPEND_PLACE":
+      // case "ADD_CITY":
+      //       return {
+      //         ...state,
+      //         city: action.payload
+      //       };
+      // case "PREPEND_PLACE":
+      //   return {
+      //     ...state,
+      //     places: [...state.places, action.payload]
+      //   };
+      // case "DELETE_PLACE":
+      //   return {
+      //     ...state,
+      //     places: state.places.filter(place => place.id !== action.payload)
+      //   };
+      // case "ADD_DESTINATIONS":
+      //   return {
+      //     ...state,
+      //     //* Ici cela marche mais cela ne fonctionne pas lorsqu'on essaye avec payload il y a une nuance avec payload il ne semble pas récupérer la data
+      //     destinations: [...state.destinations, {
+      //       city: state.city,
+      //       places: state.places
+      //     }],
+      //     city: null,
+      //     places: []
+      //   };
+      case "ADD_DESTINATION":
         return {
           ...state,
-          places: [...state.places, action.payload]
-        };
-      case "DELETE_PLACE":
-        return {
-          ...state,
-          places: state.places.filter(place => place.id !== action.payload)
-        };
-      case "ADD_DESTINATIONS":
-        return {
-          ...state,
-          //* Ici cela marche mais cela ne fonctionne pas lorsqu'on essaye avec payload il y a une nuance avec payload il ne semble pas récupérer la data
-          destinations: [...state.destinations, {
-            city: state.city,
-            places: state.places
-          }],
-          city: null,
-          places: []
-        };
+          destinations: [...state.destinations, action.payload]
+        }
       default:
         return state;
     }
