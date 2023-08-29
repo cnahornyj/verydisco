352294914f63a26a63ac017445ca659d82a2848a
Clara <clara.nahornyj@hotmail.com>
2023-08-29 09:22:53 +0200

    Création de la fonction openModalWithPlace au clic sur l'icone infos d'un lieu affiche le composant PlaceModal avec les infos du lieu passés aux props

--- src/views/DestinationPage.js
+++ src/views/DestinationPage.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import Navbar from '../components/Navbar';
 import PlaceModal from '../components/PlaceModal';
 import "../style/DestinationPage.css";
+import informations_icon from "../assets/informations_grey_light_icon.png";
 //import Carousel from '../components/Carousel';
 
 class DestinationPage extends Component {
@@ -10,7 +11,10 @@ class DestinationPage extends Component {
         super(props);
         this.state = {
             destination: null,
+            isModalOpen: false,
+            activePlace: null
         }
+        this.openModalWithPlace = this.openModalWithPlace.bind(this);
     }
     render() {
         return (    
@@ -24,26 +28,29 @@ class DestinationPage extends Component {
                 </div>
                 <div className='PlacesList'>
                 {this.state.destination.places.map((place) => (
-                    <div className='Place'>
+                    <div className='Place' key={place.place_id}>
 
                         {/* Pour récupérer toutes les photos du lieu */}
                         {/* {place.photos.map((photo) =>
                             <img src={`${photo.imgUrl}`} alt="" srcset="" />
                         )} */}
 
-                        {place.photos.length > 0 ? <img src={`${place.photos[0].imgUrl}`} alt="" srcset="" /> : null}
+                        {place.photos.length > 0 ? <img src={`${place.photos[0].imgUrl}`} alt="" /> : null}
                 
                         <div className='InformationsPlace'>
                             <h3>{place.name}</h3>
-                            <p>{place.vicinity}</p>
+                            {/* <p>{place.vicinity}</p>
                             <p>{place.rating}/5 ✨</p>
-                            <p>Nombre d'avis : {place.user_ratings_total}</p>
+                            <p>Nombre d'avis : {place.user_ratings_total}</p> */}
+                            <p className='EditorialSummary'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam eius autem nihil maxime, deleniti sapiente nulla ipsa.</p>
                             <a href={`${place.website}`} target="_blank" rel="noreferrer">Site web</a>
+                            <button onClick={() => {this.openModalWithPlace(place.place_id)}}><img src={informations_icon} alt="Informations icon" className='InfosIcon'/></button>
                         </div>
                     </div>
                 ))}
                 </div>
-                <PlaceModal name="Nom du lieu" rating="4" isOpen="true" user_ratings_total="1256" formatted_address="3 rue Jules Ferry 69800 SAINT PRIEST" hours="LUNDI 12:00 - 18:00"/>
+                {/* <PlaceModal name="Nom du lieu" rating="4" isOpen="true" user_ratings_total="1256" formatted_address="3 rue Jules Ferry 69800 SAINT PRIEST" hours="Lundi 12:00 - 18:00"/> */}
+                {this.state.isModalOpen ? <PlaceModal name={this.state.activePlace.name} rating={this.state.activePlace.rating} isOpen={this.state.activePlace.opening_hours.open_now} user_ratings_total={this.state.activePlace.user_ratings_total} reviews={[this.state.activePlace.reviews]} address_components= {[this.state.activePlace.address_components]} weekday={[this.state.activePlace.opening_hours.weekday_text]}/> : null}
             </div>
         ) : null}
         </div>
@@ -80,6 +87,32 @@ class DestinationPage extends Component {
         }, 200);
         //TODO: faire appel à la fonction getPhotos de Places Photos
     }
+
+    openModalWithPlace(placeId) {
+        this.setState({ isModalOpen: true });
+        let place = this.state.destination.places.find(function(element){
+            return element.place_id === placeId;
+        });
+        console.log("Lieu clic sur infos "+place.address_components[0].long_name);
+        this.setState({ activePlace: place});
+        setTimeout(() => {
+            console.log(this.state.activePlace);
+            console.log(this.state.activePlace.name);
+            console.log(this.state.activePlace.rating);
+            console.log(this.state.activePlace.opening_hours.open_now);
+            console.log(this.state.activePlace.user_ratings_total);
+            console.log(this.state.activePlace.reviews[0].text);
+            console.log(this.state.activePlace.address_components);
+            console.log(this.state.activePlace.opening_hours.weekday_text);
+
+        }, 1000);
+        //TODO: push place into activePlace and pass informations to PlaceModal props
+    };
+
+    closeModal = () => {
+        this.setState({ isModalOpen: false });
+        this.setState({ activePlace : null});
+    };
 }
 
 const mapStateToProps = (state) => {
