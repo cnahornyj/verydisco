de725af1bb15a45a52d300c73f8f37a206c53aba
Clara <clara.nahornyj@hotmail.com>
2024-07-17 15:03:27 +0200

    Modification du composant Liste des destinations : affichage conditionnel selon si la liste est vide ou non

--- front/src/components/DestinationsList.js
+++ front/src/components/DestinationsList.js
@@ -4,27 +4,43 @@ import { connect } from "react-redux";
 import '../style/DestinationsList.css';
 
 class DestinationsList extends Component {
-
   render() {
+    const { destinations } = this.props;
+
     return (
       <div className="DestinationsList">
         <div className="HeaderDestinationList">
           <h1>Vos destinations</h1>
-          <Link to={"/differentform"} className="redirection">
+          <Link to="/differentform" className="redirection">
             + Créer une destination
           </Link>
         </div>
         <div className="List">
-        {this.props.destinations.map((destination) => (
-          <div className="Destination">
-            {/*TODO: afficher la première image récupéré (URL stocké lorsque l'utilisateur sauvegarde sa destination)   */}
-            <img src={`https://flagsapi.com/${destination.places[0].address_components[destination.places[0].address_components.length-2].short_name}/flat/64.png`} alt="Drapeau" className="Flag"/>
-            <p key={destination.city}>
-              {destination.city.toUpperCase()}{" "}
-            </p>
-            <Link to={`/destination/${destination.city}`}>i</Link>
-          </div>
-        ))}
+          {destinations && destinations.length > 0 ? (
+            destinations.map((destination) => (
+              <div className="Destination" key={destination.city}>
+                {/* Affichage de la première image récupérée */}
+                {/* ⚠️ La valeur en BDD est address il faudra récupérer le pays dans celle-ci en conséquence */}
+                {destination.places.length > 0 && destination.places[0].address_components ? (
+                  <img
+                    src={`https://flagsapi.com/${destination.places[0].address_components[destination.places[0].address_components.length - 2].short_name}/flat/64.png`}
+                    alt="Drapeau"
+                    className="Flag"
+                  />
+                ) : (
+                  <img
+                    src="https://via.placeholder.com/64"
+                    alt="Placeholder"
+                    className="Flag"
+                  />
+                )}
+                <p>{destination.city.toUpperCase()}</p>
+                <Link to={`/destination/${destination.city}`}>i</Link>
+              </div>
+            ))
+          ) : (
+            <p>Aucune destination trouvée.</p>
+          )}
         </div>
       </div>
     );
@@ -33,7 +49,7 @@ class DestinationsList extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    destinations: state.destinations,
+    destinations: state.destinations.destinations || [], // Assurez-vous que 'destinations' est défini
   };
 };
 
